652f9919c82c3d6f07eabf2f29fd4dd5853a399a
Jordan Edwards <jordan.edwards@crwn.ai>
2025-05-18 11:18:49 -0700

    added horn

--- horn_and_light.py
+++ horn_and_light.py
@@ -2,16 +2,31 @@
 # -*- coding: utf-8 -*-
 #
 #  horn_and_light.py
-#
-
 import time
+import os
 import RPi.GPIO as GPIO
+from pydub import AudioSegment
+from pydub.playback import play
+from dotenv import load_dotenv
+
+load_dotenv()
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.OUT) # This is pin 8, GPIO 14
 
 
-def play():
+def run():
     GPIO.output(8, GPIO.HIGH)  # Turn on the LED
-    time.sleep(3)
+    try:
+        sound = AudioSegment.from_wav("horns/" + os.getenv('GOAL_HORN'))
+        play(sound)
+    except FileNotFoundError:
+        print(f"Error: File not found: horns/{os.getenv('GOAL_HORN')}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    time.sleep(15)
     GPIO.output(8, GPIO.LOW)
-    time.sleep(3)
+    return None
+
+
+run()
